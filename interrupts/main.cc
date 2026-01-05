d1f7a3d2ffca1d092861d8f2ff835302aadbdccf
Dan Green <danngreen1@gmail.com>
2026-01-04 23:31:18 -0800

    enable some interrupts

--- interrupts/main.cc
+++ interrupts/main.cc
@@ -1,12 +1,31 @@
 #include "drivers/interrupt.hh"
 #include "print.hh"
-#include <cstdint>
 
 int main()
 {
 	print("Interrupt test\n");
 
+	IRQ_Initialize();
+
+	mdrivlib::InterruptManager::register_and_start_isr(SGI1_IRQn, 0, 0, []() {
+		print("SGI1 handler started\n");
+		print("SGI1 handler ended\n");
+	});
+
+	mdrivlib::InterruptManager::register_and_start_isr(SGI2_IRQn, 0, 0, []() {
+		print("SGI2 handler started\n");
+		print("SGI2 handler ended\n");
+	});
+
+	mdrivlib::InterruptManager::register_and_start_isr(SGI3_IRQn, 0, 0, []() {
+		print("SGI3 handler started\n");
+		print("SGI3 handler ended\n");
+	});
+
+	// GIC_SendSGI
+
 	while (true) {
+		asm("nop");
 	}
 }
 
