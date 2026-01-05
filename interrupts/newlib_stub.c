d927e43ddd456a0ca24ec52f88f0f72c928fefc9
Dan Green <danngreen1@gmail.com>
2026-01-04 21:46:30 -0800

    Add more newlib stubs

--- interrupts/newlib_stub.c
+++ interrupts/newlib_stub.c
@@ -1,2 +1,28 @@
-void _init(void)
-{}
+__attribute__((used)) void _init(void) {
+}
+
+__attribute__((used)) void _fini(void) {
+}
+
+__attribute__((used)) void _exit(int x) {
+	// puts("_exit\n");
+	while (1)
+		;
+}
+
+__attribute__((used)) int _kill(int x) {
+	// puts("_kill\n");
+	return -1;
+}
+
+__attribute__((used)) int _getpid() {
+	return -1;
+}
+
+__attribute__((used)) int *__errno() {
+	static int errno = 0;
+	return &errno;
+}
+
+void *__dso_handle = (void *)&__dso_handle;
+
