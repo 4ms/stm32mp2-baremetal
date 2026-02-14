5fd2bdb6f4a794221098ca69b20dd35b0bdf8d86
Dan Green <danngreen1@gmail.com>
2026-02-13 22:37:44 -0800

    EL3 startup supported
    Only EL3 and EL1 are allowed in the program. EL2 drops to EL1, and EL0
    is not allowed at all.

--- hal-dma/startup.s
+++ hal-dma/startup.s
@@ -8,24 +8,9 @@ _Reset:
     // Mask interrupts
     msr     daifset, #0xf
 
-	// Assume we start in EL1
-    bl early_print_el
-
-el1_entry:
     // Set up stack
     ldr     x1, =_stack_start
-    mov     sp, x1
-
-    // Install vector table for EL1
-    ldr     x0, =vectors
-    msr     vbar_el1, x0
-    isb
-
-    // Enable FP+SIMD at EL1
-	mrs     x1, cpacr_el1
-	orr	    x1, x1, #3 << 20        /* FPEN bits: don't trap FPU at EL1 or EL0 */
-	msr	    cpacr_el1, x1
-	isb
+    bic     sp, x1, 0xf
 
     // Clear .bss
     ldr     x0, =_bss_start
@@ -38,19 +23,70 @@ bss_loop:
     b       bss_loop
 bss_done:
 
-    bl system_init
+    bl      early_print_el
+    mrs     x0, CurrentEL
+    lsr     x0, x0, #2
+    and     x0, x0, #0x3
+    cmp     x0, #3
+    b.eq    el3_entry
+    cmp     x0, #2
+    b.eq    el2_entry
+    cmp     x0, #1
+    b.eq    el1_entry
 
-	mrs x1, sctlr_el1
-	orr x1, x1, #0x1000    /* I: bit 12 instruction cache */
-	orr x1, x1, #0x0001    /* M: bit 1  MMU enable for EL1 and EL0  */
-	orr x1, x1, #0x0004    /* C: bit 2  Cacheability control for data accesses at EL1 and EL0 */
-	msr	sctlr_el1, x1
+//EL0 entry: hang -- not supported
+el0_entry:
+    b       el0_entry 
 
-	bl __libc_init_array
+// EL2 entry: drop down to EL1
+el2_entry:
+    mov     x2, #(0x5)              // EL1h
+    orr     x2, x2, #(0xF << 6)     // mask D,A,I,F
+    msr     spsr_el2, x2
+    // Set ELR_EL2 to EL1 entry point and provide SP_EL1
+    adr     x3, el1_entry
+    msr     elr_el2, x3
+    msr     sp_el1, x1
+    // Ensure EL1 is AArch64 (RW=1 in HCR_EL2)
+    mrs     x1, hcr_el2
+    orr     x1, x1, #(1 << 31)      // HCR_EL2.RW = 1 => EL1 is AArch64
+    msr     hcr_el2, x1
+    isb
+    eret
 
+// EL1 entry:
+el1_entry:
+    // Install vector table for EL1
+    ldr     x0, =vectors
+    msr     vbar_el1, x0
+    isb
+
+    // Enable FP+SIMD at EL1
+    mrs     x1, cpacr_el1
+    orr     x1, x1, #3 << 20        /* FPEN bits: don't trap FPU at EL1 or EL0 */
+    msr     cpacr_el1, x1
+    isb
+    bl      mmu_enable
+    b       entry_done
+
+// EL3 entry:
+el3_entry:
+    ldr     x0, =vectors
+    msr     vbar_el3, x0
+    isb
+    mrs     x0, cptr_el3
+    bic     x0, x0, #(1 << 10)        // TFP = 0 (bit 10 on many cores)
+    msr     cptr_el3, x0
+    isb
+    msr     fpcr, xzr
+    msr     fpsr, xzr
+    bl      mmu_enable_el3
+    b       entry_done
+
+entry_done:
+    bl      __libc_init_array
     msr     daifclr, #0xf
-
-    // Call main
+    bl      IRQ_Initialize
     bl      main
 
 hang:
