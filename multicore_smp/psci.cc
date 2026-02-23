f6d3e29cf2970ae9ea4949d8ef1a689ade39cde0
Dan Green <danngreen1@gmail.com>
2026-02-23 09:20:58 -0800

    Implement aux processor boot when not using SMC via secure monitor

--- multicore_smp/psci.cc
+++ multicore_smp/psci.cc
@@ -1,5 +1,6 @@
 #include "aarch64/system_reg.hh"
 #include "drivers/smc.hh"
+#include "stm32mp257cxx_ca35.h"
 #include <cstdint>
 
 #define PSCI_CPU_ON_AARCH64 0xC4000003ULL
@@ -12,7 +13,20 @@ int start_cpu1(void (*cpu1_entry)(void), uint64_t context)
 	// (AFF0 is bits[7:0] in MPIDR on Armv8-A)
 	uint64_t target = (mpidr0 & ~0xFFULL) | 1ULL;
 
-	auto ret = smc_call(PSCI_CPU_ON_AARCH64, target, (uint64_t)cpu1_entry, context, 0, 0, 0, 0);
+	if (get_current_el() == 3) {
+		// Set reset vector for CPU1 in 64-bit mode
+		CA35SYSCFG->VBAR_CR = ((uint32_t)(uintptr_t)cpu1_entry) & ~0b11;
 
-	return ret.a0; // 0 means success
+		// Reset CPU1 processor core 1
+		RCC->C1P1RSTCSETR = RCC_C1P1RSTCSETR_C1P1RST;
+		while (RCC->C1P1RSTCSETR & RCC_C1P1RSTCSETR_C1P1RST) {
+			;
+		}
+
+		return 0;
+	} else {
+		auto ret = smc_call(PSCI_CPU_ON_AARCH64, target, (uint64_t)cpu1_entry, context, 0, 0, 0, 0);
+
+		return ret.a0; // 0 means success
+	}
 }
