010ab2204fb6a94ecede4bfd746d8451633fd45b
Dan Green <danngreen1@gmail.com>
2026-02-25 10:44:52 -0800

    Add RCC PLLx setup driver

---
+++ shared/drivers/rcc_pll.hh
@@ -0,0 +1,54 @@
+#pragma once
+#include "drivers/rcc.hh"
+#include <cstdint>
+
+namespace RCC_Clocks
+{
+
+// clang-format off
+template<typename PLLT> struct MuxSel {};
+template<> struct MuxSel<PLL1> { using Mux = MuxSelPLL1; };
+template<> struct MuxSel<PLL2> { using Mux = MuxSelPLL2; };
+template<> struct MuxSel<PLL3> { using Mux = MuxSelPLL3; };
+template<> struct MuxSel<PLL4> { using Mux = MuxSelPLL4; };
+template<> struct MuxSel<PLL5> { using Mux = MuxSelPLL5; };
+template<> struct MuxSel<PLL6> { using Mux = MuxSelPLL6; };
+template<> struct MuxSel<PLL7> { using Mux = MuxSelPLL7; };
+template<> struct MuxSel<PLL8> { using Mux = MuxSelPLL8; };
+// clang-format on
+
+struct PLLSettings {
+	MuxSelSource src = MuxSelSource::hse;
+	uint16_t mult = 1;	  // 12 valid bits
+	uint8_t refdiv = 1;	  // 7 valid bits
+	uint8_t postdiv1 = 1; // 3 valid bits
+	uint8_t postdiv2 = 1; // 3 valid bits
+	uint32_t frac = 0;	  // 24 valid bits
+
+	constexpr uint32_t calc_freq(uint32_t ref_clk) const
+	{
+		return (float(ref_clk) / refdiv) * (float(mult) + frac / float(0x1000000)) / (postdiv1 * postdiv2);
+	}
+};
+
+template<typename PLLT>
+inline void set_pll(PLLSettings v)
+{
+	PLLT::Enable::clear();
+	while (PLLT::Enable::read())
+		;
+
+	MuxSel<PLLT>::Mux::write(v.src);
+	PLLT::FBDIVMult::write(v.mult);
+	PLLT::FREFDiv::write(v.refdiv);
+	PLLT::PostDiv1::write(v.postdiv1);
+	PLLT::PostDiv2::write(v.postdiv2);
+	PLLT::PostDivEnable::set();
+	PLLT::FracMult::write(v.frac);
+
+	PLLT::Enable::set();
+	while (!PLLT::Enable::read())
+		;
+}
+
+} // namespace RCC_Clocks
