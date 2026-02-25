a98ea159b2d72cf3712616f9f589d15f7d9601d8
Dan Green <danngreen1@gmail.com>
2026-02-25 10:55:51 -0800

    Add button1 to button driver (requires enabling vddio3)

--- shared/drivers/button.hh
+++ shared/drivers/button.hh
@@ -1,4 +1,19 @@
+#include "drivers/pwr_vdd.hh"
 #include "stm32mp2xx.h"
+
+inline void button_user1_init()
+{
+	PowerControl::enable_vddio3(PowerControl::Present::Always);
+	// Enable RCC for GPIOD
+	RCC->GPIODCFGR |= RCC_GPIODCFGR_GPIOxEN;
+	// PD2 mode = input (0b00)
+	GPIOD->MODER = (GPIOD->MODER & ~(0b11 << (2 * 2))) | (0b00 << (2 * 2));
+}
+
+inline bool button_user1_pressed()
+{
+	return (GPIOD->IDR & (1 << 2));
+}
 
 inline void button_user2_init()
 {
