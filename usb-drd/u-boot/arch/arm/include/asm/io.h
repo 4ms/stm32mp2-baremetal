9214b40e279bd71ec95aa6126f60ee63d610d49f
Dan Green <danngreen1@gmail.com>
2026-03-10 22:04:31 -0700

    Copy readl and writel from io.h in uboot

--- usb-drd/u-boot/arch/arm/include/asm/io.h
+++ usb-drd/u-boot/arch/arm/include/asm/io.h
@@ -4,19 +4,27 @@
 #include <linux/compiler.h>
 #include <linux/types.h>
 
-static inline u32 readl(const volatile void __iomem *addr)
-{
-	u32 val;
-	__asm__ volatile("ldr %w0, [%1]" : "=r"(val) : "r"(addr) : "memory");
-	return val;
-}
+// static inline u32 readl(const volatile void __iomem *addr)
+// {
+// 	u32 val;
+// 	__asm__ volatile("ldr %w0, [%1]" : "=r"(val) : "r"(addr) : "memory");
+// 	return val;
+// }
 
-static inline void writel(u32 val, volatile void __iomem *addr)
-{
-	__asm__ volatile("str %w0, [%1]" : : "r"(val), "r"(addr) : "memory");
-}
+// static inline void writel(u32 val, volatile void __iomem *addr)
+// {
+// 	__asm__ volatile("str %w0, [%1]" : : "r"(val), "r"(addr) : "memory");
+// }
 
-#define readl_relaxed(addr) readl(addr)
-#define writel_relaxed(val, addr) writel(val, addr)
+// #define readl_relaxed(addr) readl(addr)
+// #define writel_relaxed(val, addr) writel(val, addr)
+
+// From u-boot:
+#define __iormb()	asm("dmb sy")
+#define __iowmb()	asm("dmb sy")
+#define __arch_getl(a)			(*(volatile unsigned int *)(a))
+#define __arch_putl(v,a)		(*(volatile unsigned int *)(a) = (v))
+#define readl(c)	({ u32 __v = __arch_getl(c); __iormb(); __v; })
+#define writel(v,c)	({ u32 __v = v; __iowmb(); __arch_putl(__v,c); __v; })
 
 #endif /* _ASM_IO_H */
