43e18e4263be92e28be846c7af7ca8208ea4af21
Dan Green <danngreen1@gmail.com>
2026-03-12 14:09:36 -0700

    Disable pr_dbg and pr_vdbg output

--- usb-drd/u-boot/include/dm/device_compat.h
+++ usb-drd/u-boot/include/dm/device_compat.h
@@ -3,13 +3,14 @@
 
 #include <stdio.h>
 
-#define dev_dbg(dev, fmt, ...) printf("[DWC3 DBG] " fmt, ##__VA_ARGS__)
-#define dev_vdbg(dev, fmt, ...) printf("[DWC3 DVG] " fmt, ##__VA_ARGS__)
+// #define dev_dbg(dev, fmt, ...) printf("[DWC3 DBG] " fmt, ##__VA_ARGS__)
+// #define dev_vdbg(dev, fmt, ...) printf("[DWC3 DVG] " fmt, ##__VA_ARGS__)
+
 #define dev_info(dev, fmt, ...) printf("[DWC3 INF] " fmt, ##__VA_ARGS__)
 #define dev_warn(dev, fmt, ...) printf("[DWC3 WRN] " fmt, ##__VA_ARGS__)
 #define dev_err(dev, fmt, ...) printf("[DWC3 ERR] " fmt, ##__VA_ARGS__)
 
-// #define dev_dbg(dev, fmt, ...)
-// #define dev_vdbg(dev, fmt, ...)
+#define dev_dbg(dev, fmt, ...)
+#define dev_vdbg(dev, fmt, ...)
 
 #endif /* _DM_DEVICE_COMPAT_H */
