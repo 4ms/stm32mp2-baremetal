f2635a2e4fa47e99307232b7cef74a5ae8c4f351
Dan Green <danngreen1@gmail.com>
2026-03-13 16:51:59 -0700

    Less log output

--- usb-drd/u-boot/include/log.h
+++ usb-drd/u-boot/include/log.h
@@ -3,12 +3,12 @@
 
 #include <stdio.h>
 
-#define pr_debug(fmt, ...)	printf("[DWC3 DBG] " fmt, ##__VA_ARGS__)
-#define pr_info(fmt, ...)	printf("[DWC3 INF] " fmt, ##__VA_ARGS__)
-#define pr_warn(fmt, ...)	printf("[DWC3 WRN] " fmt, ##__VA_ARGS__)
-#define pr_err(fmt, ...)	printf("[DWC3 ERR] " fmt, ##__VA_ARGS__)
+#define pr_debug(fmt, ...)	do {} while (0)
+#define pr_info(fmt, ...)	do {} while (0)
+#define pr_warn(fmt, ...)	printf("[WRN] " fmt, ##__VA_ARGS__)
+#define pr_err(fmt, ...)	printf("[ERR] " fmt, ##__VA_ARGS__)
 
-/* U-Boot's debug() macro — used by linux-compat.h's dev_WARN */
-#define debug(fmt, ...)		printf("[DWC3 DBG] " fmt, ##__VA_ARGS__)
+/* U-Boot's debug() macro — used throughout xHCI/DWC3 code */
+#define debug(fmt, ...)		do {} while (0)
 
 #endif /* _LOG_H */
